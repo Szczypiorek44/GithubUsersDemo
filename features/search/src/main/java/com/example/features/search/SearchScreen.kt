c1f5dd728a9a9d320eb1f5de244fb24da546b486
Karol Michalski <karol.michalski95@gmail.com>
2024-03-07 17:22:36 +0100

    Adapt search list item to the designs

--- features/search/src/main/java/com/example/features/search/SearchScreen.kt
+++ features/search/src/main/java/com/example/features/search/SearchScreen.kt
@@ -3,10 +3,12 @@ package com.example.features.search
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -15,13 +17,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
 import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.common.theme.GithubUsersTheme
@@ -81,36 +87,61 @@ fun UserItem(
             .wrapContentHeight()
             .fillMaxWidth()
     ) {
-        val (name, country, state) = createRefs()
+        val (name, country, state, divider) = createRefs()
 
         Text(
             modifier = Modifier.constrainAs(name) {
-                top.linkTo(parent.top, margin = 16.dp)
+                top.linkTo(parent.top, margin = 12.dp)
+                start.linkTo(parent.start, margin = 12.dp)
             },
-            text = user.name,
+            text = user.name.uppercase(),
             fontWeight = FontWeight.Medium,
-            color = Gray,
-            fontSize = 20.sp
+            color = DarkGray,
+            fontSize = 18.sp
         )
+
+        val verticalGuideline = createGuidelineFromStart(0.5f)
 
         Text(
             modifier = Modifier.constrainAs(state) {
-                top.linkTo(name.bottom, margin = 16.dp)
+                start.linkTo(name.start)
+                end.linkTo(verticalGuideline)
+                top.linkTo(name.bottom, margin = 6.dp)
+                width = Dimension.fillToConstraints
             },
             text = user.stateName,
-            color = Black,
-            fontSize = 16.sp
+            color = Gray,
+            fontSize = 16.sp,
+            maxLines = 1
         )
 
         Text(
-            modifier = Modifier.constrainAs(country) {
-                end.linkTo(parent.end, margin = 16.dp)
-                bottom.linkTo(state.bottom, margin = 5.dp)
-            },
+            modifier = Modifier
+                .constrainAs(country) {
+                    start.linkTo(verticalGuideline, margin = 4.dp)
+                    end.linkTo(parent.end, margin = 12.dp)
+                    bottom.linkTo(state.bottom, margin = 2.dp)
+                    width = Dimension.fillToConstraints
+                },
             text = user.countryName,
-            color = Black,
-            fontSize = 16.sp
+            color = Gray,
+            fontSize = 16.sp,
+            maxLines = 1,
+            textAlign = TextAlign.End,
+            overflow = TextOverflow.Ellipsis
         )
+
+        Divider(
+            modifier = Modifier
+                .constrainAs(divider) {
+                    top.linkTo(state.bottom, margin = 10.dp)
+                    bottom.linkTo(parent.bottom)
+                }
+                .fillMaxWidth()
+                .height(1.dp),
+            color = LightGray
+        )
+
     }
 }
 
@@ -131,4 +162,10 @@ val fakeUserList = listOf(
     User(3, "Bob", countryName = "United States", stateName = "Ohio"),
     User(4, "Julia", countryName = "Colombia", stateName = "Cundinamarca"),
     User(5, "Barbara", countryName = "Poland", stateName = "Mazowieckie"),
+    User(
+        6,
+        "Clare",
+        countryName = "Ecuador (Santo Domingo)",
+        stateName = "Some state with long name"
+    ),
 )
